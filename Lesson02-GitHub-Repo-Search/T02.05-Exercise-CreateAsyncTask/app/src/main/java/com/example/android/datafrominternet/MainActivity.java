805d21b288c05147900bc2adc2b9f8add2a3e10a
Pilot92802 <colby.downsbb@yahoo.com>
2022-08-20 09:24:40 -0500

    WV: replaced AsynchTask with java.concurrent Executor

--- Lesson02-GitHub-Repo-Search/T02.05-Exercise-CreateAsyncTask/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ Lesson02-GitHub-Repo-Search/T02.05-Exercise-CreateAsyncTask/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -15,8 +15,12 @@
  */
 package com.example.android.datafrominternet;
 
+
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Handler;
+import android.os.Looper;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
@@ -26,6 +30,8 @@ import com.example.android.datafrominternet.utilities.NetworkUtils;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView mUrlDisplayTextView;
 
     private TextView mSearchResultsTextView;
+
+    private String githubSearchResults = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,25 +58,80 @@ public class MainActivity extends AppCompatActivity {
      * This method retrieves the search text from the EditText, constructs the URL
      * (using {@link NetworkUtils}) for the github repository you'd like to find, displays
      * that URL in a TextView, and finally fires off an AsyncTask to perform the GET request using
-     * our (not yet created) {@link GithubQueryTask}
+     * our (not yet created) {at link GithubQueryTask}
      */
     private void makeGithubSearchQuery() {
         String githubQuery = mSearchBoxEditText.getText().toString();
         URL githubSearchUrl = NetworkUtils.buildUrl(githubQuery);
         mUrlDisplayTextView.setText(githubSearchUrl.toString());
-        String githubSearchResults = null;
-        try {
+       /* try {
             githubSearchResults = NetworkUtils.getResponseFromHttpUrl(githubSearchUrl);
             mSearchResultsTextView.setText(githubSearchResults);
         } catch (IOException e) {
             e.printStackTrace();
-        }
-        // TODO (4) Create a new GithubQueryTask and call its execute method, passing in the url to query
+        }*/
+        // TODO (4) [Done] Create a new GithubQueryTask and call its execute method, passing in the url to query
+        //new GithubQueryTask().execute(githubSearchUrl);
+
+        // Asynctask has been fully deprecated... need to find a new way to accomplish multi thread operation.
+        /* found information on Java.Util.Concurrent.  This package allows us to create ExecutorServices which
+           will help us execute more time intensive tasks off of the main thread
+        */
+
+        // TODO (5) [Done] Create and instance of the ExecutorService class
+        // TODO (6) [Done] in that Executor Service call the URL search routine
+        // TODO (7) [Done] Create an instance of the Handler class
+        // TODO (8) [Done] Use the Handler to update the search results text view
+
+        Executor searchThread = Executors.newSingleThreadExecutor();
+        Handler resultHandler = new Handler(Looper.getMainLooper());
+
+        searchThread.execute(new Runnable(){
+            @Override
+            public void run(){
+                try {
+                    githubSearchResults =NetworkUtils.getResponseFromHttpUrl(githubSearchUrl);
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+
+                resultHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mSearchResultsTextView.setText(githubSearchResults);
+                    }
+                });
+
+            }
+        });
+
+
     }
 
-    // TODO (1) Create a class called GithubQueryTask that extends AsyncTask<URL, Void, String>
-    // TODO (2) Override the doInBackground method to perform the query. Return the results. (Hint: You've already written the code to perform the query)
-    // TODO (3) Override onPostExecute to display the results in the TextView
+    // TODO (1) [Done] Create a class called GithubQueryTask that extends AsyncTask<URL, Void, String>
+    // TODO (2) [Done] Override the doInBackground method to perform the query. Return the results. (Hint: You've already written the code to perform the query)
+    // TODO (3) [Done] Override onPostExecute to display the results in the TextView
+    /*public class GithubQueryTask extends AsyncTask <URL , Void, String>{
+        @Override
+        protected String doInBackground(URL... urls){
+            URL searchURL = urls[0];
+            String githubSearchResults = null;
+            try{
+                githubSearchResults = NetworkUtils.getResponseFromHttpUrl(searchURL);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return githubSearchResults;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if (s != null && !s.equals("")){
+                mSearchResultsTextView.setText(s);
+            }
+        }
+    }*/
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
